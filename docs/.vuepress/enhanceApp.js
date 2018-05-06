2db643a578e7aeb56b42bd747a1f5858223d8269
hughfenghen <hughfenghen@gmail.com>
2018-05-06 20:50:28 +0800

    集成 评论模块 gitment

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,52 @@
+function integrateGitment(router) {
+  const linkGitment = document.createElement('link')
+  linkGitment.href = 'https://imsun.github.io/gitment/style/default.css'
+  linkGitment.rel = 'stylesheet'
+  const scriptGitment = document.createElement('script')
+  document.body.appendChild(linkGitment)
+  scriptGitment.src = 'https://imsun.github.io/gitment/dist/gitment.browser.js'
+  document.body.appendChild(scriptGitment)
+
+  router.afterEach((to) => {
+    // 已被初始化则根据页面重新渲染 评论区
+    if (scriptGitment.onload) {
+      renderGitment(to.fullPath)
+    } else {
+      scriptGitment.onload = () => {
+        const commentsContainer = document.createElement('div')
+        commentsContainer.id = 'comments-container'
+        commentsContainer.classList.add('content')
+        document.querySelector('.page').appendChild(commentsContainer)
+
+        renderGitment(to.fullPath)
+      }
+    }
+  })
+
+  function renderGitment(fullPath) {
+    const gitment = new Gitment({
+      id: fullPath,
+      owner: 'hughfenghen',
+      repo: 'hughfenghen.github.io',
+      oauth: {
+        client_id: 'e157002b5da973611b6c',
+        client_secret: 'c8659321b3c44cf1cc4f429a067d99d681bdb9e1',
+      },
+    })
+    gitment.render('comments-container')
+  }
+}
+
+export default ({
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData // 站点元数据
+}) => {
+  // ...做一些其他的应用级别的优化
+  try {
+    document && integrateGitment(router)
+  } catch (e) {
+    console.error(e.message)
+  }
+}
