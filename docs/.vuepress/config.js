9a8acd18e6b7c58231cd3db64ab5f1078eea6872
hughfenghen <hughfenghen@gmail.com>
2018-05-12 15:25:50 +0800

    启用了fenghen.xyz域名, coding.net 不需要发布到子目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,10 @@ module.exports = {
   title: '风痕的博客',
   description: '...',
   ga: 'UA-118782515-1',
-  base: process.env.VP_DEPLOY_ENV === 'coding.net'
-    ? '/hughfenghen.github.io/'
-    : '/',
+  // 启用了fenghen.xyz域名, coding.net 不需要发布到子目录
+  // base: process.env.VP_DEPLOY_ENV === 'coding.net'
+  //   ? '/hughfenghen.github.io/'
+  //   : '/',
   themeConfig: {
     sidebar: {
       '/': [{
