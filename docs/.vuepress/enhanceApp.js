f49da62248e04c46c0442c91fbdc4ee20616d41e
hughfenghen <hughfenghen@gmail.com>
2021-02-13 12:59:15 +0800

    接入gitalk

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -13,15 +13,15 @@ function tryRun (fn, times = 3) {
   }
 }
 
-function integrateGitment(router) {
-  const linkGitment = document.createElement('link')
-  linkGitment.href = 'https://imsun.github.io/gitment/style/default.css'
-  linkGitment.rel = 'stylesheet'
-  const scriptGitment = document.createElement('script')
-  scriptGitment.src = 'https://imsun.github.io/gitment/dist/gitment.browser.js'
+function integrateGitalk(router) {
+  const linkGitalk = document.createElement('link')
+  linkGitalk.href = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css'
+  linkGitalk.rel = 'stylesheet'
+  const scriptGitalk = document.createElement('script')
+  scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js'
 
-  document.body.appendChild(linkGitment)
-  document.body.appendChild(scriptGitment)
+  document.body.appendChild(linkGitalk)
+  document.body.appendChild(scriptGitalk)
 
   router.afterEach((to, from) => {
     // 页面滚动，hash值变化，也会触发afterEach钩子，避免重新渲染
@@ -30,11 +30,11 @@ function integrateGitment(router) {
     // 已被初始化则根据页面重新渲染 评论区
     tryRun((next) => {
       const $page = document.querySelector('.page')
-      if ($page && window.Gitment) {
+      if ($page && window.Gitalk) {
         // gitment 取document.title作为issue的标题
         // 如果不setTimeout取到是上一篇文档的标题
         setTimeout(() => {
-          renderGitment($page, to.path)
+          renderGitalk($page, to.path)
         }, 1);
       } else {
         next(500)
@@ -42,27 +42,28 @@ function integrateGitment(router) {
     }, 10)
   })
 
-  function renderGitment(parentEl, path) {
+  function renderGitalk(parentEl, path) {
     // 移除旧节点，避免页面切换 评论区内容串掉
-    const oldEl = document.getElementById('comments-container');
+    const oldEl = document.getElementById('gitalk-container');
     oldEl && oldEl.parentNode.removeChild(oldEl);
 
     const commentsContainer = document.createElement('div')
-    commentsContainer.id = 'comments-container'
+    commentsContainer.id = 'gitalk-container'
     commentsContainer.classList.add('content')
-    commentsContainer.style = 'padding: 0 30px;'
+    commentsContainer.style = 'max-width: 740px; margin: 0 auto;'
     parentEl.appendChild(commentsContainer)
-    const gitment = new Gitment({
-      id: path,
-      owner: 'hughfenghen',
+    var gitalk = new Gitalk({
+      clientID: '8a03da926cf95085e3cc',
+      clientSecret: '1b9a0256e3ac0a88ff287df6582d06c7806d017a',
       repo: 'hughfenghen.github.io',
-      link: location.origin + path,
-      oauth: {
-        client_id: 'e157002b5da973611b6c',
-        client_secret: 'c8659321b3c44cf1cc4f429a067d99d681bdb9e1',
-      },
+      owner: 'hughfenghen',
+      admin: ['hughfenghen'],
+      proxy: 'https://netnr-proxy.cloudno.de/https://github.com/login/oauth/access_token',
+      id: location.pathname,      // Ensure uniqueness and length less than 50
+      distractionFreeMode: false  // Facebook-like distraction free mode
     })
-    gitment.render('comments-container')
+
+    gitalk.render('gitalk-container')
   }
 }
 
@@ -74,7 +75,7 @@ export default ({
 }) => {
   try {
     // 生成静态页时在node中执行，没有document对象
-    document && integrateGitment(router)
+    document && integrateGitalk(router)
 
     addSiteMap(siteData, router)
   } catch (e) {
