e9874668efcea82bd3f94e3d8981ee0311f3dea6
hughfenghen <hughfenghen@gmail.com>
2022-09-19 15:11:41 +0800

    fix: github 禁止push master 分支

--- docs/deploy-github.sh
+++ docs/deploy-github.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:hughfenghen/hughfenghen.github.io.git master
+git push -f git@github.com:hughfenghen/hughfenghen.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
