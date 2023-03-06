f7aac88f85376a09de42dae0a3321ae25c14311b
hughfenghen <hughfenghen@gmail.com>
2023-03-06 11:51:52 +0800

    优化 site-map 生成，解决重定向导致不被google收录

--- docs/.build/gen-site-map.js
+++ docs/.build/gen-site-map.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const vpCfg = require('../.vuepress/config')
-const { __, pipe, flatten, values, map, get, add, join } = require('lodash/fp')
+const { tap, pipe, flatten, values, map, get, add } = require('lodash/fp')
 
 const siteMap = pipe(
   get('themeConfig.sidebar'),
@@ -9,6 +9,8 @@ const siteMap = pipe(
   flatten,
   map(get('children')),
   flatten,
+  // 单独 md 没有 .html 结尾，vuepress 会有一次重定向，导致 Google 拒绝收录
+  map(v => v.endsWith('/') ? v : v + '.html'),
   map(add('https://hughfenghen.github.io')),
 )(vpCfg)
 
