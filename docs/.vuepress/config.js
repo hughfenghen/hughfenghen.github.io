28e0fcba2a5d44d3203fc99224202e7ce88fb820
hughfenghen <hughfenghen@gmail.com>
2023-04-26 10:37:40 +0800

    chore: 更新 经验随记 链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
       rel: '',
     }, {
       text: '经验随记',
-      link: 'https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85',
+      link: 'https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85%2C%E8%AF%97%E8%AF%8D',
       target: '_self',
       rel: '',
     }, {
--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 
 Mail: <a href="mailto:hughfenghen@gmail.com">hughfenghen@gmail.com</a>
 
-[经验随记](https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85)  
+[经验随记](https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85%2C%E8%AF%97%E8%AF%8D)  
 
 [CSDN 旧博客](https://blog.csdn.net/lj745280746)
 
