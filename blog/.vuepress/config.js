34592c5db1fc7f2b47d4cb2360e6fa3b7c6157c6
hughfenghen <hughfenghen@gmail.com>
2024-01-31 17:24:07 +0800

    集成 clarity

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -3,6 +3,13 @@ module.exports = {
   title: '风痕 · 術&思',
   description: '个人思考、Web技术',
   theme: '@vuepress/blog',
+  head: [['script', {}, `
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "kudfzh7lis");
+  `]],
   plugins: [
     ['copy-code1', {
       showInMobile: true,
