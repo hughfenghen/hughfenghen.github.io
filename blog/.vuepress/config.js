59b9e75ecfbfd2e3a4afd774d204d3056aff77d9
hughfenghen <hughfenghen@gmail.com>
2024-02-19 14:23:59 +0800

    更新  分页数量，随记链接

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -54,6 +54,9 @@ module.exports = {
     lineNumbers: false
   },
   themeConfig: {
+    globalPagination: {
+      lengthPerPage: 10
+    },
     directories: [
       {
         id: "posts",
@@ -77,7 +80,7 @@ module.exports = {
       },
       {
         text: "随记",
-        link: "https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85%2C%E8%AF%97%E8%AF%8D%2CVssue"
+        link: "https://github.com/hughfenghen/hughfenghen.github.io/issues?q=-label%3AGitalk%2C%E5%BF%83%E6%83%85%2C%E8%AF%97%E8%AF%8D%2CVssue+is%3Aopen+"
       },
       {
         text: "Github",
