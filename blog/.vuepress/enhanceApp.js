d35f55235126f47d3e14b853e690f7776e060fc3
hughfenghen <hughfenghen@gmail.com>
2025-10-10 22:04:38 +0800

    fix: 评论区无法显示

--- blog/.vuepress/enhanceApp.js
+++ blog/.vuepress/enhanceApp.js
@@ -1,7 +1,13 @@
 
 // ---- 解决 URL 中文 404 start -----
 import Router from 'vue-router'
-function decode (str) {
+
+// 兼容某些依赖报错，需要 global
+if (typeof window !== "undefined")
+  window.global = window;
+
+
+function decode(str) {
   try {
     return decodeURIComponent(str)
   } catch (err) {
@@ -13,7 +19,7 @@ function decode (str) {
 }
 
 const VueRouterMatch = Router.prototype.match
-Router.prototype.match = function match (raw, currentRoute, redirectedFrom) {
+Router.prototype.match = function match(raw, currentRoute, redirectedFrom) {
   if (typeof raw === 'string') {
     raw = decode(raw)
   }
@@ -32,35 +38,35 @@ export default ({
     let path = null
 
     if (to.path.includes('/fe/body-mask-danmaku')) {
-      path = '/posts/2023/06/21/body-mask-danmaku/' 
+      path = '/posts/2023/06/21/body-mask-danmaku/'
     } else if (to.path.includes('/fe-basic-course/js-lang-history-and-basic')) {
-      path = '/posts/2020/07/29/js-lang-history-and-basic/' 
+      path = '/posts/2020/07/29/js-lang-history-and-basic/'
     } else if (to.path.includes('/fe-basic-course/js-concurrent')) {
-      path = '/posts/2023/03/27/js-concurrent/' 
+      path = '/posts/2023/03/27/js-concurrent/'
     } else if (to.path.includes('/other/diy-handheld-game')) {
-      path = '/posts/2023/06/21/diy-handheld-game/' 
+      path = '/posts/2023/06/21/diy-handheld-game/'
     } else if (to.path.includes('/fe-basic-course/options-request')) {
-      path = '/posts/2023/03/22/options-request/' 
+      path = '/posts/2023/03/22/options-request/'
     } else if (to.path.includes('/think/qin')) {
-      path = '/posts/2020/10/07/qin/' 
+      path = '/posts/2020/10/07/qin/'
     } else if (to.path.includes('/fe-basic-course/js-data-process')) {
-      path = '/posts/2020/07/29/js-data-process/' 
+      path = '/posts/2020/07/29/js-data-process/'
     } else if (to.path.includes('/fe/js-perf-detect')) {
-      path = '/posts/2023/04/03/js-perf-detect/' 
+      path = '/posts/2023/04/03/js-perf-detect/'
     } else if (to.path.includes('/fe-basic-course/ts-types-system')) {
-      path = '/posts/2023/03/27/ts-types-system/' 
+      path = '/posts/2023/03/27/ts-types-system/'
     } else if (to.path.includes('/think/dao')) {
-      path = '/posts/2020/07/13/dao/' 
+      path = '/posts/2020/07/13/dao/'
     } else if (to.path.includes('/think/biosphere')) {
-      path = '/posts/2020/10/07/biosphere/' 
+      path = '/posts/2020/10/07/biosphere/'
     } else if (to.path.includes('/fe/vscode-shortcut')) {
-      path = '/posts/2023/03/29/vscode-shortcut/' 
+      path = '/posts/2023/03/29/vscode-shortcut/'
     }
 
-    if (path != null) { 
+    if (path != null) {
       location.href = path
       return false
     }
-    else { next() } 
+    else { next() }
   })
 }
